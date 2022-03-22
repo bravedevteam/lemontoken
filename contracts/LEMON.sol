7cee864417d56918bcacffb5c39e95e4208cddd5
Namhoon Lee <frontalnh@gmail.com>
2022-03-22 18:41:48 +0900

    update

--- contracts/LEMON.sol
+++ contracts/LEMON.sol
@@ -667,6 +667,44 @@ library Roles {
     }
 }
 
+/**
+ * @dev Extension of `KIP7` that allows token holders to destroy both their own
+ * tokens and those that they have an allowance for, in a way that can be
+ * recognized off-chain (via event analysis).
+ *
+ * See http://kips.klaytn.com/KIPs/kip-7-fungible_token
+ */
+contract KIP7Burnable is KIP13, KIP7 {
+    /*
+     *     bytes4(keccak256('burn(uint256)')) == 0x42966c68
+     *     bytes4(keccak256('burnFrom(address,uint256)')) == 0x79cc6790
+     *
+     *     => 0x42966c68 ^ 0x79cc6790 == 0x3b5a0bf8
+     */
+    bytes4 private constant _INTERFACE_ID_KIP7BURNABLE = 0x3b5a0bf8;
+
+    constructor () public {
+        // register the supported interfaces to conform to KIP17 via KIP13
+        _registerInterface(_INTERFACE_ID_KIP7BURNABLE);
+    }
+    
+    /**
+     * @dev Destroys `amount` tokens from the caller.
+     *
+     * See `KIP7._burn`.
+     */
+    function burn(uint256 amount) public {
+        _burn(msg.sender, amount);
+    }
+
+    /**
+     * @dev See `KIP7._burnFrom`.
+     */
+    function burnFrom(address account, uint256 amount) public {
+        _burnFrom(account, amount);
+    }
+}
+
 contract PauserRole {
     using Roles for Roles.Role;
 
@@ -816,44 +854,6 @@ contract KIP7Pausable is KIP13, KIP7, Pausable {
 
 
 /**
- * @dev Extension of `KIP7` that allows token holders to destroy both their own
- * tokens and those that they have an allowance for, in a way that can be
- * recognized off-chain (via event analysis).
- *
- * See http://kips.klaytn.com/KIPs/kip-7-fungible_token
- */
-contract KIP7Burnable is KIP13, KIP7, Pausable {
-    /*
-     *     bytes4(keccak256('burn(uint256)')) == 0x42966c68
-     *     bytes4(keccak256('burnFrom(address,uint256)')) == 0x79cc6790
-     *
-     *     => 0x42966c68 ^ 0x79cc6790 == 0x3b5a0bf8
-     */
-    bytes4 private constant _INTERFACE_ID_KIP7BURNABLE = 0x3b5a0bf8;
-
-    constructor () public {
-        // register the supported interfaces to conform to KIP17 via KIP13
-        _registerInterface(_INTERFACE_ID_KIP7BURNABLE);
-    }
-    
-    /**
-     * @dev Destroys `amount` tokens from the caller.
-     *
-     * See `KIP7._burn`.
-     */
-    function burn(uint256 amount) public whenNotPaused {
-        _burn(msg.sender, amount);
-    }
-
-    /**
-     * @dev See `KIP7._burnFrom`.
-     */
-    function burnFrom(address account, uint256 amount) public whenNotPaused {
-        _burnFrom(account, amount);
-    }
-}
-
-/**
  * @dev Optional functions from the KIP7 standard.
  * See http://kips.klaytn.com/KIPs/kip-7-fungible_token
  */
@@ -991,7 +991,7 @@ contract Ownable {
     }
 }
 
-contract LEMON is KIP7Pausable, KIP7Burnable, KIP7Metadata, Ownable {
+contract LEMON is KIP7Burnable, KIP7Pausable, KIP7Metadata, Ownable {
     constructor() KIP7Metadata("LEMON", "LEMN", 18) public {
         _mint(msg.sender, 1000000000000000000000000000);
     }
